540b8b158ceea7a1bc7bc33fcebe1f00e68f45af
Tay <tmitsumori96@gmail.com>
2022-01-06 22:27:36 -0800

    Removed Mongodb, replaced with SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, url_for, redirect
+from flask.scaffold import F
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    desp = db.Column(db.String(length=1500), nullable=False)
+
+    def __repr__(self):
+        #override default storage name in database
+        return f'Product {self.name}'
+
 
 @app.route('/')
 @app.route('/home')
@@ -11,12 +26,18 @@ def home():
 @app.route('/products')
 def products():
     '''Show ALL products'''
-    products = [
-        {'name': 'Classic', 'price': 25.99, 'desp': 'Our classic flow'},
-        {'name': 'Medium', 'price': 29.99, 'desp': 'Medium'},
-        {'name': 'Locked In', 'price': 29.99, 'desp': 'Lock in your flow for a 24 hour hold'}
-    ]
+    products = Product.query.all()
+    #products = [
+        #{'name': 'Classic', 'price': 25.99, 'desp': 'Our classic flow'},
+        #{'name': 'Medium', 'price': 29.99, 'desp': 'Medium'},
+        #{'name': 'Locked In', 'price': 29.99, 'desp': 'Lock in your flow for a 24 hour hold'}
+    #]
     return render_template('products.html', products=products)
+
+@app.route('/signin')
+def signin():
+    """Sign into EXISTING account"""
+    return redirect('/account')
 
 if __name__ == '__main__':
     app.run(debug=True)
