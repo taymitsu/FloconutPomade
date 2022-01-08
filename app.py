c9eda878d9f3d7ead685d8ba7d887b818567341a
Tay <tmitsumori96@gmail.com>
2022-01-07 22:54:37 -0800

    Create Account functions added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask.scaffold import F
 from flask_sqlalchemy import SQLAlchemy
+from forms import RegisterForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SECRET_KEY'] = '622a05368bddd00c722f371a'
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -44,6 +46,12 @@ def products():
 def signin():
     """Sign into EXISTING account"""
     return redirect('/account')
+
+@app.route('register')
+def register():
+    """Create new account"""
+    form = RegisterForm()
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
